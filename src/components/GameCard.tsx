10cdb3586cdfd8b174a24e2ca9d90e22affb1096
dhsbfhjsbfjsh <AzureVoyager2024@proton.me>
2024-03-12 20:30:59 +0800

    Displaying platform icons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Game } from "../hooks/useGames";
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
+import PlatformlconList from "./PlatformlconList";
 
 interface props {
   game: Game;
@@ -12,6 +13,9 @@ const GameCard = ({ game }: props) => {
       <Image src={game.background_image} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
+        <PlatformlconList
+          platforms={game.parent_platforms.map((p) => p.platform)}
+        />
       </CardBody>
     </Card>
   );
