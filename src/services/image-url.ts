d9f49eb2a030090e9b159833c8a5d5d7c6c5cd3f
dhsbfhjsbfjsh <AzureVoyager2024@proton.me>
2024-03-15 16:57:00 +0800

    Handle  games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,6 +1,8 @@
+import noImage from '../assets/no-image-placeholder.wepp2024-03-15上午12.21.59.png';
+
 
 const getCroppedImageUrl = (url: string) =>  {
-    if (!url) return '';
+    if (!url) return noImage;
     const target = 'media/';
     const index = url.indexOf(target) + target.length;
     return url.slice(0,index) + 'crop/600/400/' + url.slice(index);     
